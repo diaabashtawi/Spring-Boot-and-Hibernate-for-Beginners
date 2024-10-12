534a10c75ee8c3a3dc9e63f8d77ab7d0015bd0c6
diaabashtawi <diaabashtawi@gmail.com>
2024-10-12 21:38:15 +0400

    adding S6-Spring-MVC project

---
+++ S6-Spring-MVC/src/main/java/com/bakheet/mvc/S6SpringMcvApplication.java
@@ -0,0 +1,13 @@
+package com.bakheet.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S6SpringMcvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S6SpringMcvApplication.class, args);
+	}
+
+}
---
+++ S6-Spring-MVC/src/test/java/com/bakheet/mvc/S6SpringMcvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakheet.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S6SpringMcvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
